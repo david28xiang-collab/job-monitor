4c65951343fd7f06c51c46c666ed95268a5d6808
david28xiang-collab <david28xiang@gmail.com>
2026-06-11 14:22:26 -0500

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,13 +1,7 @@
 import requests
-import re
 
 html = requests.get(
-    "https://analystcareers-analysisgroup.icims.com/jobs/search"
+    "https://www.crai.com/cra-careers/jobs/"
 ).text
 
-matches = re.findall(r'/jobs/\d+/[^"]+', html)
-
-for m in matches:
-    print(m)
-
-print("count:", len(matches))
+print(html[:5000])
