0e9d55214196d49126a5a6d2b5e60eaa3f86a5a6
david28xiang-collab <david28xiang@gmail.com>
2026-06-11 14:10:12 -0500

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -3,5 +3,7 @@ import yaml
 with open("companies.yaml", "r") as f:
     data = yaml.safe_load(f)
 
+print("Companies being monitored:")
+
 for company in data["companies"]:
-    print(company["name"])
+    print(company)
