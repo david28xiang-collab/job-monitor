ccd1c2462ea45d15b7a707ba6e8cfc8341d760bb
DavidinDaBuilding <97921386+DavidinDaBuilding@users.noreply.github.com>
2026-06-11 19:17:06 -0500

    changed

--- discord_notify.py
+++ discord_notify.py
@@ -1,9 +1,9 @@
 import requests
 import os
 
-webhook_url = os.environ["DISCORD_WEBHOOK"]
 
 def send_discord(message):
+    webhook_url = os.environ["DISCORD_WEBHOOK"]
 
     requests.post(
         webhook_url,
