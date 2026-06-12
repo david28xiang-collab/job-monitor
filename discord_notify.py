cf73739289695d8e48fc886927825db3f392dc4f
DavidinDaBuilding <97921386+DavidinDaBuilding@users.noreply.github.com>
2026-06-11 19:19:25 -0500

    changed

--- discord_notify.py
+++ discord_notify.py
@@ -3,7 +3,7 @@ import os
 
 
 def send_discord(message):
-    webhook_url = os.environ["DISCORD_WEBHOOK"]
+    webhook_url = "https://discord.com/api/webhooks/1514779296458346600/INtAL1KPXJy1ZSQgMrkYDEu44KGzGwmxA5S6flkXtzHTyr3-1BoGn85z6So6VHR0nBSa"
 
     requests.post(
         webhook_url,
