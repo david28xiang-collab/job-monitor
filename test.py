58ec436dcfb54e7beddbb97702a041c856d73c25
DavidinDaBuilding <97921386+DavidinDaBuilding@users.noreply.github.com>
2026-06-24 14:28:09 -0500

    changed

--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-import requests
+from discord_notify import send_discord
 
-url = (
-    "https://fticonsulting.wd108.myworkdayjobs.com"
-    "/wday/cxs/fticonsulting"
-    "/CompassLexeconCareers/jobs"
-)
-
-r = requests.post(
-    url,
-    json={
-        "limit": 20,
-        "offset": 0
-    }
-)
-
-print(r.status_code)
-print(r.text[:1000])
+send_discord("Hello from Job Monitor")
