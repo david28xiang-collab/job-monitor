f13492eed7b0446c5ddecb7ff3724854bc97f1ce
DavidinDaBuilding <97921386+DavidinDaBuilding@users.noreply.github.com>
2026-06-24 18:19:16 -0500

    changed

--- greenhouse.py
+++ greenhouse.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def fetch_greenhouse(board, company):
 
     url = f"https://boards-api.greenhouse.io/v1/boards/{board}/jobs"
@@ -10,14 +11,23 @@ def fetch_greenhouse(board, company):
 
     for job in data["jobs"]:
 
+        title = job["title"]
+
+        # Skip internship positions
+        if any(keyword in title.lower() for keyword in [
+            "intern",
+            "internship",
+            "co-op",
+            "coop"
+        ]):
+            continue
+
         jobs.append({
             "company": company,
             "job_id": str(job["id"]),
-            "title": job["title"],
+            "title": title,
             "location": job.get("location", {}).get("name"),
             "url": job["absolute_url"]
         })
 
     return jobs
-
-    
