6cb009701bed5b1216da51846d6bca30e2bcd544
DavidinDaBuilding <97921386+DavidinDaBuilding@users.noreply.github.com>
2026-08-10 11:17:50 -0500

    changed

--- daily_run.py
+++ daily_run.py
@@ -1,15 +1,18 @@
 import subprocess
+import sys
+
 
 print("Running monitor...")
+
 subprocess.run(
-["python", "monitor.py"],
-check=True
+    [sys.executable, "monitor.py"],
+    check=True
 )
+
 
 print("Running compare...")
-subprocess.run(
-["python", "compare.py"],
-check=True
-)
 
-print("Done.")
+subprocess.run(
+    [sys.executable, "compare.py"],
+    check=True
+)
